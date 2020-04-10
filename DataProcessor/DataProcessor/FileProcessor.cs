f868d5ab28481e3e0e0c5f4f7487b92bae242a25
Carlos Ramírez <calbertora@gmail.com>
2020-04-10 10:14:01 -0500

    Initial commit

---
+++ DataProcessor/DataProcessor/FileProcessor.cs
@@ -0,0 +1,95 @@
+﻿using static System.Console;
+using System.IO;
+using System;
+
+namespace DataProcessor
+{
+    internal class FileProcessor
+    {
+        private static readonly string BackupDirectoryName = "backup";
+        private static readonly string InProgressDirectoryName = "processing";
+        private static readonly string CompletedDirectoryName = "complete";
+        public string InputFilePath { get; }
+
+        public FileProcessor(string filePath)
+        {
+            this.InputFilePath = filePath;
+        }
+
+        public void Process()
+        {
+            WriteLine($"Begin process of {this.InputFilePath}");
+
+            if(!File.Exists(this.InputFilePath))
+            {
+                WriteLine($"Error: file {InputFilePath} does not exists");
+                return;
+            }
+
+            string rootDirectoryPath = new DirectoryInfo(InputFilePath).Parent.Parent.FullName;
+            WriteLine($"Root data path is {rootDirectoryPath}");
+
+            string inputFileDirectoryPath = Path.GetDirectoryName(InputFilePath);
+            string backupDirectoryPath = Path.Combine(rootDirectoryPath, BackupDirectoryName);
+
+            if(!Directory.Exists(backupDirectoryPath))
+            {
+                WriteLine($"Creating {backupDirectoryPath}");
+                Directory.CreateDirectory(backupDirectoryPath);
+            }
+
+            string inputFileName = Path.GetFileName(InputFilePath);
+            string backupFilePath = Path.Combine(backupDirectoryPath, inputFileName);
+
+            WriteLine($"Copying {InputFilePath} to {backupFilePath}");
+
+            File.Copy(InputFilePath,backupFilePath, true);
+
+            Directory.CreateDirectory(Path.Combine(rootDirectoryPath, InProgressDirectoryName));
+
+            string inProgressFilePath =
+                Path.Combine(rootDirectoryPath, InProgressDirectoryName, inputFileName);
+
+            if(File.Exists(inProgressFilePath))
+            {
+                WriteLine($"ERROR: a file with the name {inProgressFilePath} is already being processed");
+                return;
+            }
+
+            WriteLine($"Moving {InputFilePath} to {inProgressFilePath}");
+            File.Move(InputFilePath, inProgressFilePath);
+
+            string extension = Path.GetExtension(InputFilePath);
+
+            switch(extension)
+            {
+                case ".txt":
+                    ProcessTextFile(inProgressFilePath);
+                    break;
+                default:
+                    WriteLine($"{extension} is an unsopported file type");
+                    break;
+            }
+
+            string completedDirectoryPath = Path.Combine(rootDirectoryPath, CompletedDirectoryName);
+            Directory.CreateDirectory(completedDirectoryPath);
+
+            WriteLine($"Moving {inProgressFilePath} to {completedDirectoryPath}");
+
+            var completedFileName =
+                $"{Path.GetFileNameWithoutExtension(InputFilePath)}-{Guid.NewGuid()}{extension}";
+
+            completedFileName = Path.ChangeExtension(completedFileName, ".complete");
+            var completedFilePath = Path.Combine(completedDirectoryPath, completedFileName);
+            File.Move(inProgressFilePath, completedFilePath);
+
+            string inProgressDirectoryPath = Path.GetDirectoryName(inProgressFilePath);
+            Directory.Delete(inProgressDirectoryPath);
+        }
+
+        private void ProcessTextFile(string inProgressFilePath)
+        {
+            WriteLine($"Processing text file {inProgressFilePath}");
+        }
+    }
+}
